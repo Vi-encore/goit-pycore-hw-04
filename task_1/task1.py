58cb10f2c2a2340f6282d9c89fe15744a0befc85
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-03-14 15:41:21 +0200

    task1 fix

--- task_1/task1.py
+++ task_1/task1.py
@@ -11,16 +11,13 @@ def total_salary(path):
           print(f'Невірний формат рядка: {line.strip()}')
           # salaries.append(0) #result will be incorrect in the end (more employees than money)
           # pass #again incorrect results
-
           return(error) #will break def
         
       total = sum(salaries) 
       average = total//len(salaries) if salaries else 0
 
       return total, average
-    
-    
-      # return(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {total//num_of_employees}")
+      # return(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
     
   except FileNotFoundError as error:
     print('Такого файлу не існує!')
@@ -30,18 +27,18 @@ def total_salary(path):
     return(error)
 
 
-
-
-
 # path = 'task_1\\salary.txt' #relative
-total, average = total_salary("task_1\\salary.txt")
-
 # print(total_salary(path))
+
+total, average = total_salary("task_1\\salary.txt")
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
 
-#you can swap all uncommented lines for commented => will still work 
 
-#because uncommented lines cause 2 errors - one from try...except block
+#If Values are faulty => will return 2 errors (i break def purposely)
 
+# One - error from try...except block
 #Other one will be TypeError - because of return error inside for cycle 
 #If I swap that return with pass or append => result will be incorrect, so I decided to break function instead
+
+#so to prevent this from happening I left a couple of lines commented
+#Swap them with uncommented and will give only one error
