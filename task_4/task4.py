efdd3a7805490421a985109539aa215dd6a6e5a3
Viktoriia Matiushko <vikamatuchko772@gmail.com>
2025-03-15 20:11:49 +0200

    task4 partly done

--- task_4/task4.py
+++ task_4/task4.py
@@ -5,30 +5,72 @@ def parse_input(user_input):
     return cmd, *args
 
 def add_contact(args, contacts):
+    if len(args) != 2:
+        return "Invalid command format. Use: add [name] [phone]"
+    
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
+def show_all(contacts):
+    if len(contacts) == 0:
+        return 'Contact list is empty'   
+    
+    result = []
+    for contact in contacts:
+        result.append(f'{contact}: {contacts[contact]}')
+    return '\n'.join(result)
+
+def change_contact(args, contacts):
+        if len(args) != 2:
+            return "Invalid command format. Use: add [name] [new phone]"
+
+        name, phone = args
+        if name not in contacts:
+            return f'There are no contacts with name {name}'
+        
+        contacts[name] = phone
+        return 'Contact changed.'
+    
+
+def phone_user(args, contacts):
+    if len(args) != 1:
+        return "Invalid command format. Use: phone [name]"
+    
+    name = args[0]
+    if name not in contacts:
+        return f'There are no contacts with name {name}'
+    
+    return contacts[name]
 
 
 def main():
-    contacts = {}
+    contacts = {'Vi': 345,
+                'Dsa': 45346}
+    # contacts = {'Vi': 4535,}
     print("Welcome to the assistant bot!")
 
     while True:
-      user_input = input("Enter a command: ")
-      command, *args = parse_input(user_input)
-      if command in ["close", "exit"]:
-          print("Good bye!")
-          break
-      elif command == "hello":
-          print("How can I help you?")
-      elif command == "add":
-          print(add_contact(args, contacts))
-      else:
-          print("Invalid command.")
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == 'change':
+            print(change_contact(args, contacts))
+        elif command == 'phone':
+            print(phone_user(args, contacts))
+        elif command == 'all':
+            print(show_all(contacts))
+        else:
+            print("Invalid command.")
 
 
 
 if __name__ == '__main__':
-  main()
+    main()
